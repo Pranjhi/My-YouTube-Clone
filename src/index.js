a6c175f5624fa26b35e5fe77c75fd6a9a07e6bb8
Pranjhi Singh <133675758+Pranjhi@users.noreply.github.com>
2023-08-28 23:47:36 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export{default as Navbar}from './Navbar';
+export{default as Feed}from './Feed';
+export{default as ChannelDetail}from './ChannelDetail';
+export{default as SearchFeed}from './SearchFeed';
+export{default as VideoDetails}from './VideoDetails';
+export{default as SearchBar}from './SearchBar';
+export{default as Sidebar}from './Sidebar';
+export{default as Videos}from './Videos';
+export{default as VideoCard}from './VideoCard';
+export{default as ChannelCard}from './ChannelCard';
